2bd83c9d9c0b7c557684d2b9d433840c2bc9b5a4
royweb <xytank@gmail.com>
2022-04-27 13:19:03 -0400

    Add SubInfo to NFT card

--- components/NFTCard.js
+++ components/NFTCard.js
@@ -4,6 +4,7 @@ import React from 'react';
 
 import { COLORS, SIZES, SHADOWS, assets } from '../constants';
 import { CircleButton } from './Button';
+import { SubInfo, EthPrice, NFTTitle } from './SubInfo';
 
 const NFTCard = ({ data }) => {
   const navigation = useNavigation();
@@ -30,6 +31,7 @@ const NFTCard = ({ data }) => {
 
         <CircleButton imgUrl={assets.heart} right={10} top={10} />
       </View>
+      <SubInfo />
     </View>
   )
 }
