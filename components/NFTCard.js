22a1bd7430dacc8a5e4780cbc5d234e3ec832640
royweb <xytank@gmail.com>
2022-04-28 14:05:09 -0400

    Add EthPrice to NFT card

--- components/NFTCard.js
+++ components/NFTCard.js
@@ -40,6 +40,16 @@ const NFTCard = ({ data }) => {
           subTitle={data.creator}
           titleSize={SIZES.large}
           subTitleSize={SIZES.small} />
+
+        <View
+          style={{
+            marginTop: SIZES.font,
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center'
+          }}>
+            <EthPrice price={data.price} />
+        </View>
       </View>
     </View>
   )
