041e34c208c6ef27f652b81f076d43ae2d5202c3
royweb <xytank@gmail.com>
2022-05-05 14:08:26 -0400

    Add bottom button to Details page

--- screens/Details.js
+++ screens/Details.js
@@ -14,6 +14,18 @@ const Details = ({ route, navigation }) => {
         barStyle="dark-content"
         backgroundColor="transparent"
         translucent={true} />
+
+      <View style={{
+        width: '100%',
+        position: 'absolute',
+        bottom: 0,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'rgba(255, 255, 255, 0.5)',
+        zIndex: 1
+      }}>
+        <RectButton minWidth={170} fontSize={SIZES.large} {...SHADOWS.dark} />
+      </View>
     </SafeAreaView>
   )
 }
