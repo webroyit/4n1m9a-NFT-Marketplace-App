4a75f5bcd1e39a051ae2a3e37144bf885e2fb276
royweb <xytank@gmail.com>
2022-05-06 15:16:44 -0400

    Add DetailsDesc to Details page

--- components/DetailsDesc.js
+++ components/DetailsDesc.js
@@ -1,12 +1,49 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from 'react-native';
+import React, { useState } from 'react';
 
-const DetailsDesc = () => {
+import { EthPrice, NFTTitle } from './SubInfo';
+import { COLORS, SIZES, FONTS } from '../constants';
+
+const DetailsDesc = ({ data }) => {
   return (
     <View>
-      <Text>DetailsDesc</Text>
+      <View style={{
+        width: '100%',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+      }}>
+        <NFTTitle
+          title={data.name}
+          subtitle={data.creator}
+          titleSize={SIZES.extraLarge}
+          subTitleSize={SIZES.font} />
+
+        <EthPrice price={data.price} />
+      </View>
+
+      <View style={{ marginVertical: SIZES.extraLarge * 1.5 }}>
+        <Text style={{
+          fontSize: SIZES.font,
+          fontFamily: FONTS.semiBold,
+          color: COLORS.primary
+        }}>
+          Description
+        </Text>
+
+        <View style={{ marginTop: SIZES.base}}>
+          <Text style={{
+            fontSize: SIZES.small,
+            fontFamily: FONTS.regular,
+            color: COLORS.secondary,
+            lineHeight: SIZES.large
+          }}>
+            {data.description}
+          </Text>
+        </View>
+      </View>
     </View>
   )
 }
 
-export default DetailsDesc
+export default DetailsDesc;
--- components/index.js
+++ components/index.js
@@ -4,7 +4,8 @@ import NFTCard from "./NFTCard";
 import { CircleButton, RectButton } from "./Button";
 import DetailsDesc from "./DetailsDesc";
 import DetailsBid from "./DetailsBid";
+import { SubInfo, People, EndDate, EthPrice, NFTTitle } from "./SubInfo";
 
 export {
-  HomeHeader, NFTCard, FousedStatusBar, CircleButton, RectButton, DetailsDesc, DetailsBid
+  HomeHeader, NFTCard, FousedStatusBar, CircleButton, RectButton, DetailsDesc, DetailsBid, SubInfo,  People, EndDate, EthPrice, NFTTitle
 }
