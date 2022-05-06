cab6b63d5ea5317fd79ea0b0b0877eb4275b074c
royweb <xytank@gmail.com>
2022-05-06 15:57:20 -0400

    Add read more for description

--- components/DetailsDesc.js
+++ components/DetailsDesc.js
@@ -5,6 +5,9 @@ import { EthPrice, NFTTitle } from './SubInfo';
 import { COLORS, SIZES, FONTS } from '../constants';
 
 const DetailsDesc = ({ data }) => {
+  const [text, setText] = useState(data.description.slice(0, 100));
+  const [readMore, setReadMore] = useState(false);
+
   return (
     <View>
       <View style={{
@@ -38,7 +41,25 @@ const DetailsDesc = ({ data }) => {
             color: COLORS.secondary,
             lineHeight: SIZES.large
           }}>
-            {data.description}
+            {text}
+            {!readMore && '...'}
+            <Text
+              style={{
+                fontSize: SIZES.small,
+                fontFamily: FONTS.semiBold,
+                color: COLORS.primary,
+              }}
+              onPress={() => {
+                if(!readMore) {
+                  setText(data.description);
+                  setReadMore(true);
+                } else {
+                  setText(data.description.slice(0, 100));
+                  setReadMore(false);
+                }
+              }}>
+              {readMore ? '  Show Less': '  Read More'}
+            </Text>
           </Text>
         </View>
       </View>
